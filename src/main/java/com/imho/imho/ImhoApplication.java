191d7031bd0c7327d7d684fac4ad6f3e916239c6
kimsoohwan <user@DESKTOP-NEP71KT>
2023-07-14 17:13:46 +0900

    최초 프로젝트 생성

---
+++ src/main/java/com/imho/imho/ImhoApplication.java
@@ -0,0 +1,13 @@
+package com.imho.imho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imho/imho/ImhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imho.imho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
